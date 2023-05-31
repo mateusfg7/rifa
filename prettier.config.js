4472dea2e3cc527632200829998a87ebd31b1e59
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-05-31 15:14:00 -0300

    chore(prettier): create config file

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'none',
+  endOfLine: 'auto',
+}
