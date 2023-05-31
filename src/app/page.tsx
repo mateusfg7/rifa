20ff7b15787c72b4561dc21f575f6a08c8fe4ceb
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-05-31 16:54:09 -0300

    feat(ui): add initial form and card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,142 @@
-import Image from 'next/image'
+'use client'
+
+import { useState } from 'react'
+
+interface RequestBody {
+  name: string
+  phone: string
+  address: string
+  paymentReceiptUrl: string
+  amount: number
+}
 
 export default function Home() {
+  const [name, setName] = useState('')
+  const [phone, setPhone] = useState('')
+  const [address, setAddress] = useState('')
+  const [paymentDate, setPaymentDate] = useState('')
+  const [paymentReceiptUrl, setPaymentReceiptUrl] = useState('')
+  const [amount, setAmount] = useState(1)
+
+  const paymentDateAsDate = new Date(paymentDate)
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="flex min-h-screen w-screen items-center justify-center gap-10 border border-red-600">
+      <div className="item-center flex flex-1 flex-col items-center justify-center space-y-10 rounded-xl bg-gradient-to-r  from-sky-50 to-indigo-50 p-10 shadow-xl">
+        <h1 className="text-2xl font-bold underline">Cadastrar nova rifa</h1>
+        <form className="w-full space-y-10 border">
+          <div className="flex gap-2">
+            <div className="flex-1 space-y-1">
+              <h2 className="text-lg font-bold">Nome</h2>
+              <input
+                type="text"
+                value={name}
+                onChange={e => {
+                  setName(e.target.value)
+                }}
+                className="w-full rounded-lg border border-black/50 p-3 active:border-black"
+                placeholder="Nome Completo do Comprador"
+              />
+            </div>
+            <div className="space-y-1">
+              <h2 className="text-lg font-bold">Telefone</h2>
+              <input
+                type="text"
+                value={phone}
+                onChange={e => {
+                  setPhone(e.target.value)
+                }}
+                className="rounded-lg border border-black/50 p-3 text-center active:border-black"
+                placeholder="(99) 99999-9999"
+              />
+            </div>
+          </div>
+          <div className="space-y-1">
+            <h2 className="text-lg font-bold">Endereço</h2>
+            <input
+              type="text"
+              value={address}
+              onChange={e => {
+                setAddress(e.target.value)
+              }}
+              className="w-full rounded-lg border border-black/50 p-3 active:border-black"
+              placeholder="Rua Exemplo, 0000 - Bairro"
             />
-          </a>
+          </div>
+          <div className="flex gap-2">
+            <div className="flex-1 space-y-1">
+              <h2 className="text-lg font-bold">Comprovante de pagamento</h2>
+              <input
+                type="text"
+                value={paymentReceiptUrl}
+                onChange={e => {
+                  setPaymentReceiptUrl(e.target.value)
+                }}
+                className="w-full rounded-lg border border-black/50 p-3 active:border-black"
+                placeholder="https://linkdocomprovante.com/comprovante.png"
+              />
+            </div>
+            <div className="space-y-1">
+              <h2 className="text-lg font-bold">Data de pagamento</h2>
+              <input
+                type="date"
+                value={paymentDate}
+                onChange={e => {
+                  setPaymentDate(e.target.value)
+                }}
+                className="rounded-lg border border-black/50 p-3 active:border-black"
+                placeholder="https://linkdocomprovante.com/comprovante.png"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <h2 className="text-lg font-bold">Qtd de números</h2>
+            <input
+              type="number"
+              value={amount}
+              onChange={e => {
+                setAmount(Number(e.target.value))
+              }}
+              className="w-16 rounded-lg border border-black/50 p-1 text-center active:border-black"
+            />
+          </div>
+        </form>
+      </div>
+      <div className="flex-1">
+        <div className="flex flex-col gap-5 rounded-xl border bg-gradient-to-r from-sky-500 to-indigo-500 p-5 text-white shadow-xl">
+          <h1 className="text-3xl font-bold italic tracking-wide underline">
+            Maleta Camarim
+          </h1>
+          <div className="flex gap-5 text-lg">
+            <div className="font-bold">
+              <div>Nome</div>
+              <div>Endereço</div>
+              <div>Telefone</div>
+              <div>Valor</div>
+              <div>Data do pagamento</div>
+            </div>
+            <div>
+              <div>{name.length > 0 ? name : 'Nome do Comprador'}</div>
+              <div>
+                {address.length > 0 ? address : 'Rua Exemplo, 0000 - Bairro'}
+              </div>
+              <div>{phone.length > 0 ? phone : '(99) 99999-9999'}</div>
+              <div>R$ {amount * 10},00</div>
+              <div>
+                {paymentDate.length > 0
+                  ? `${paymentDateAsDate.getDay()}/${paymentDateAsDate.getMonth()}/${paymentDateAsDate.getFullYear()}`
+                  : '01/01/1001'}
+              </div>
+            </div>
+          </div>
+          <div>
+            <span className="text-xl font-bold tracking-wide">
+              Números: 054 056
+            </span>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
