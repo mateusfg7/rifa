6be22161c52ddbca15e409618880da4d203f880a
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-05-31 15:25:36 -0300

    chore(eslint): integrate with prettier

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'prettier'
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['react'],
+  rules: {
+    'space-before-function-paren': 'off',
+    'react/prop-types': 'off',
+    'no-use-before-define': 'off',
+    'prefer-const': 'off',
+    'react/react-in-jsx-scope': 'off'
+  }
+}
--- prettier.config.js
+++ prettier.config.js
@@ -4,5 +4,5 @@ module.exports = {
   singleQuote: true,
   arrowParens: 'avoid',
   trailingComma: 'none',
-  endOfLine: 'auto',
+  endOfLine: 'auto'
 }
