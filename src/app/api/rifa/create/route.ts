cd55cadd860a63890b13177cf92e466edfdae45b
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-06-01 11:07:53 -0300

    feat(api): update `POST /api/rifa/create`
    Connect with google-spreadsheets and Google Cloud Storage

--- src/app/api/rifa/create/route.ts
+++ src/app/api/rifa/create/route.ts
@@ -1,21 +1,40 @@
 import { NextResponse } from 'next/server'
-import { registerNewTickets } from './lib/registerNewTicket'
+
+import { registerNewTickets } from './lib/register-new-ticket'
+import { saveFileToDisk } from './lib/save-file-to-disk'
+import { uploadReceiptToGcs } from './lib/upload-receipt-to-gcs'
 
 export async function POST(request: Request) {
   const formData = await request.formData()
 
-  const body: CreateTicketBody = {
+  const { address, amount, name, paymentReceipt, phone } = {
     name: formData.get('name') as string,
     phone: formData.get('phone') as string,
     address: formData.get('address') as string,
-    paymentReceiptUrl: formData.get('paymentReceiptUrl') as string,
+    paymentReceipt: formData.get('paymentReceipt') as File,
     amount: Number(formData.get('amount'))
   }
 
+  const formattedFileName = name.toLowerCase().replaceAll(' ', '-')
+  const filePath = await saveFileToDisk(paymentReceipt, formattedFileName)
+  const date = new Date()
+  const paymentReceiptUrl = await uploadReceiptToGcs(
+    filePath,
+    `${formattedFileName}_${date.toISOString()}.${
+      paymentReceipt.type.split('/')[1]
+    }`
+  )
+
   try {
-    const tickets = await registerNewTickets(body)
+    const ticket = await registerNewTickets({
+      address,
+      amount,
+      name,
+      phone,
+      paymentReceiptUrl
+    })
     return NextResponse.json(
-      { message: 'Rifa cadastrada com sucesso', rifas: tickets },
+      { message: 'Rifa cadastrada com sucesso', rifa: ticket },
       { status: 201 }
     )
   } catch (err) {
