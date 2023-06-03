2f21830b600d24916043f3e9b4f18c157991c414
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-06-03 10:12:37 -0300

    fix(lib): try fix env variables issues

--- src/lib/load-spreadsheet-document.ts
+++ src/lib/load-spreadsheet-document.ts
@@ -3,8 +3,8 @@ import { GoogleSpreadsheet } from 'google-spreadsheet'
 export async function loadSpreadsheetDocument() {
   const doc = new GoogleSpreadsheet(process.env.DOC_ID)
   await doc.useServiceAccountAuth({
-    client_email: process.env.CLIENT_EMAIL as string,
-    private_key: process.env.PRIVATE_KEY as string
+    client_email: String(process.env.CLIENT_EMAIL),
+    private_key: String(process.env.PRIVATE_KEY?.split(String.raw`\n`).join('\n'))
   })
   await doc.loadInfo()
 
